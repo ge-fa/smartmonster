f3a248f94349cd3816286f0bb7b60fd971608cdf
Silvio Rhatto <rhatto@riseup.net>
2014-08-08 21:32:03 -0300

    Make it a puppet module

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 install:
-	cp src/file_system_reporter.sh $(DESTDIR)/usr/bin/
-	cp src/update_power_counter.sh $(DESTDIR)/usr/bin/
-	cp src/99_smartmonster_sleep.d.sh $(DESTDIR)/etc/pm/sleep.d/
+	cp files/file_system_reporter.sh $(DESTDIR)/usr/bin/
+	cp files/update_power_counter.sh $(DESTDIR)/usr/bin/
+	cp files/99_smartmonster_sleep.d.sh $(DESTDIR)/etc/pm/sleep.d/
---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+class smartmonster {
+  file { '/usr/local/sbin/file_system_reporter.sh':
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => 0755,
+    source => 'puppet:///modules/smartmonster/file_system_reporter.sh',
+  }
+
+  file { '/usr/local/sbin/update_power_counter.sh':
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/smartmonster/update_power_counter.sh',
+  }
+
+  file { '/etc/pm/sleep.d/99_smartmonster_sleep.d.sh':
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => '0755',
+    source => 'puppet:///modules/smartmonster/99_smartmonster_sleep.d.sh',
+  }
+}
